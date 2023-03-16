7af3c8be4be67073ee25ead7ad6572332ca51cf0
ANA LUIZA ELERT <ana_elert@estudante.sesisenai.org.br>
2023-03-16 13:20:27 -0300

    test4

--- jogo.js
+++ jogo.js
@@ -1,7 +1,28 @@
-// function startGame(){
-//     document.getElementById('main').style.background = "transparent";
-//     alert("Primeira dica: 😱");    
-// }
+function startGame(){
+    document.getElementById('main').style.background = "transparent";
+    document.getElementById('main').innerHTML =
+        `
+        <a-scene mindar-image="imageTargetSrc: ./targets.mind" color-space="sRGB" renderer="colorManagement: true, physicallyCorrectLights" vr-mode-ui="enabled: false" device-orientation-permission-ui="enabled: false">
+            <a-camera position="0 0 0" look-controls="enabled: false"></a-camera>
+
+            <a-entity id="img1" mindar-image-target="targetIndex: 0" ></a-entity>
+            <a-entity id="img2" mindar-image-target="targetIndex: 1" ></a-entity>
+            <a-entity id="img3" mindar-image-target="targetIndex: 2" ></a-entity>
+            <a-entity id="img4" mindar-image-target="targetIndex: 3" ></a-entity>
+            <a-entity id="img5" mindar-image-target="targetIndex: 4" ></a-entity>
+            <a-entity id="img6" mindar-image-target="targetIndex: 5" ></a-entity>
+            <a-entity id="img7" mindar-image-target="targetIndex: 6" ></a-entity>
+            <a-entity id="img8" mindar-image-target="targetIndex: 7" ></a-entity>
+            <a-entity id="img9" mindar-image-target="targetIndex: 8" ></a-entity>
+            <a-entity id="img10" mindar-image-target="targetIndex: 9"></a-entity>
+            <a-entity mindar-image-target="targetIndex: 10" id="img11"></a-entity>
+            <a-entity mindar-image-target="targetIndex: 11" id="img12"></a-entity>
+
+        </a-scene>
+        <script src="./jogo.js"></script>
+        `;
+    alert("Primeira dica: 😱");    
+}
 
 let target1 = document.querySelector('#img1');
 let target2 = document.querySelector('#img2');
@@ -16,22 +37,22 @@ let target10 = document.querySelector('#img10');
 let target11 = document.querySelector('#img11');
 let target12 = document.querySelector('#img12');
 
-// if(target1){
+if(target1){
     target1.addEventListener("targetFound", event => {
-        // setInterval(
-        //     alert("Parabéns!! Você encontrou a pintura 'O Grito' de Edvard Munch")
-        // , 5000); 
-        // localStorage.setItem("Pontuacao", 1);
+        setInterval(
+            alert("Parabéns!! Você encontrou a pintura 'O Grito' de Edvard Munch")
+        , 5000); 
+        localStorage.setItem("Pontuacao", 1);
         alert("Próxima dica:  🌻");
     });
-// }
+}
 
 if(target2){
     target2.addEventListener("targetFound", event  => {
         setInterval(
             alert("Parabéns!! Você encontrou a pintura 'Doze Girassóis' de Vincent van Gogh")
         , 5000); 
-        // localStorage.setItem("Pontuacao", 2);
+        localStorage.setItem("Pontuacao", 2);
         alert("Próxima dica:  🌃");
     });
 }
